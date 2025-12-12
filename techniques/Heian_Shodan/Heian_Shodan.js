9864fc015dc5db192a49494b0f3f1de844713883
Kandaku <kandaku@gmail.com>
2025-12-12 16:41:27 +0100

    Add files via upload

--- techniques/Heian_Shodan/Heian_Shodan.js
+++ techniques/Heian_Shodan/Heian_Shodan.js
@@ -16,7 +16,7 @@ window.COUNT = 23;
 // 3. Description (Optionnel)
 window.DESCRIPTION = `<b>Heian Shodan</b> est le premier kata de base du <b>karaté Shotokan</b>, structuré pour initier le pratiquant au travail des déplacements, des blocages et des coups de poing fondamentaux.[web:22][web:25]
 Son nom est généralement traduit par « paix et tranquillité, premier niveau », exprimant l'idée d'un esprit calme et stable au début du parcours technique du karatéka.[web:21][web:22]
-Il met l'accent sur un enchaînement clair de 21 mouvements, avec des changements de direction simples, en utilisant principalement <b>Gedan Barai</b>, <b>Oi Zuki</b>, <b>Jodan Age Uke</b> et <b>Shuto Uke</b> en positions <b>Zenkutsu Dachi</b> et <b>Ko Kutsu Dachi</b> pour construire des bases solides.[web:22][web:27]`;
+Il met l'accent sur un enchaînement clair de 21 mouvements, avec des changements de direction simples, en utilisant principalement <b>Gedan Barai</b>, <b>Oi Zuki</b>, <b>Jodan Age Uke</b> et <b>Shuto Uke</b> en positions <b>Zenkutsu Dachi</b> et <b>Kokutsu Dachi</b> pour construire des bases solides.[web:22][web:27]`;
 
 // 4. Arrière-plan (Optionnel)
 window.BACKGROUND = 1;
@@ -81,16 +81,16 @@ Heian_Shodan_18_L1 <<Oi Zuki>><Zuki>
 Heian_Shodan_18_L2 Migi <Zenkutsu Dachi>
 
 Heian_Shodan_19_L1 <Shuto Uke>
-Heian_Shodan_19_L2 Hidari <Ko Kutsu Dachi>
+Heian_Shodan_19_L2 Hidari <Kokutsu Dachi>
 
 Heian_Shodan_20_L1 <Shuto Uke>
-Heian_Shodan_20_L2 Migi <Ko Kutsu Dachi>
+Heian_Shodan_20_L2 Migi <Kokutsu Dachi>
 
 Heian_Shodan_21_L1 <Shuto Uke>
-Heian_Shodan_21_L2 Migi <Ko Kutsu Dachi>
+Heian_Shodan_21_L2 Migi <Kokutsu Dachi>
 
 Heian_Shodan_22_L1 <Shuto Uke>
-Heian_Shodan_22_L2 Hidari <Ko Kutsu Dachi>
+Heian_Shodan_22_L2 Hidari <Kokutsu Dachi>
 
 Heian_Shodan_23_L1 <Yoi>
 Heian_Shodan_23_L2 <Hachiji Dachi>
