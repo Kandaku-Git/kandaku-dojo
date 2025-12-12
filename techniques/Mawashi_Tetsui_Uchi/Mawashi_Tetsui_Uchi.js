dc320abd60be1c66e0474d0be16ccde4c9610346
Kandaku <kandaku@gmail.com>
2025-12-12 15:47:34 +0100

    Ajout de Heian Shodan

---
+++ techniques/Mawashi_Tetsui_Uchi/Mawashi_Tetsui_Uchi.js
@@ -0,0 +1,41 @@
+﻿/**
+
+* FICHIER DE CONFIGURATION DE LA TECHNIQUE : Mawashi Tetsui Uchi
+
+*/
+
+// --- 1. Extension des images ---
+// Par défaut `.gif`. Décommentez si différent.
+// window.EXTENSION = `.gif`;
+
+// --- 2. Nombre d'images ---
+window.COUNT = `3`;
+
+// --- 3. Description ---
+window.DESCRIPTION = `
+<b> Armer :</b>
+<br>
+. Le poing qui va frapper est sur la hanche opposée (pouce vers le bas) ou sur l'épaule opposée
+<br>
+. L'autre bras est tendu devant
+<br>
+<br>
+<b> Développer : </b>
+<br>
+. Le poing à la hanche décrit un arc de cercle
+<br>
+. L'autre poing vient se placer en hikite
+<br>
+<br>
+<b> Résultat : </b>
+<br>
+. L'attaque a été faite avec la base du poing (celle qui est opposée au pouce)
+<br>
+. L'autre poing est en hikite
+`;
+
+// --- 4. Données Techniques ---
+window.DONNEES = `
+FIXE_L1 Zuki Waza
+FIXE_L2 Technique de poing
+`;
