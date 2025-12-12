dc4342a9509cc829fa8d98bc12c1a82aabb55301
Kandaku <kandaku@gmail.com>
2025-12-12 16:31:31 +0100

    Modification technique

---
+++ techniques/Heian_Shodan.js
@@ -0,0 +1,97 @@
+// FICHIER DE CONFIGURATION DE LA TECHNIQUE Heian Shodan
+
+// ---------------------------------------------------------------
+// Instructions - Respectez la syntaxe window.VARIABLE = `valeur`;
+// avec les accents graves.
+// ---------------------------------------------------------------
+
+// 1. Extension des images (Optionnel)
+// Par défaut : .gif
+// Décommentez si vos images sont en .jpg ou .png
+// window.EXTENSION = `.jpg`;
+
+// 2. Nombre d'images (Obligatoire)
+window.COUNT = 23;
+
+// 3. Description (Optionnel)
+window.DESCRIPTION = `<b>Heian Shodan</b> est le premier kata de base du <b>karaté Shotokan</b>, structuré pour initier le pratiquant au travail des déplacements, des blocages et des coups de poing fondamentaux.[web:22][web:25]
+Son nom est généralement traduit par « paix et tranquillité, premier niveau », exprimant l'idée d'un esprit calme et stable au début du parcours technique du karatéka.[web:21][web:22]
+Il met l'accent sur un enchaînement clair de 21 mouvements, avec des changements de direction simples, en utilisant principalement <b>Gedan Barai</b>, <b>Oi Zuki</b>, <b>Jodan Age Uke</b> et <b>Shuto Uke</b> en positions <b>Zenkutsu Dachi</b> et <b>Kokutsu Dachi</b> pour construire des bases solides.[web:22][web:27]`;
+
+// 4. Arrière-plan (Optionnel)
+window.BACKGROUND = 1;
+
+// 5. Durée par défaut (Optionnel)
+window.SLIDEDURATION = 800;
+
+// 6. Données Techniques : Texte et Temps
+window.DONNEES = `
+Heian_Shodan_1_L1 <Yoi>
+Heian_Shodan_1_L2 <Hachiji Dachi>
+
+Heian_Shodan_2_L1 <Gedan Barai>
+Heian_Shodan_2_L2 Hidari <Zenkutsu Dachi>
+
+Heian_Shodan_3_L1 <<Oi Zuki>><Zuki>
+Heian_Shodan_3_L2 Migi <Zenkutsu Dachi>
+
+Heian_Shodan_4_L1 <Gedan Barai>
+Heian_Shodan_4_L2 Migi <Zenkutsu Dachi>
+
+Heian_Shodan_5_L1 <Mawashi Tetsui Uchi>
+Heian_Shodan_5_L2 Migi <Renoji Dachi>
+
+Heian_Shodan_6_L1 <<Oi Zuki>><Zuki>
+Heian_Shodan_6_L2 Hidari <Zenkutsu Dachi>
+
+Heian_Shodan_7_L1 <Gedan Barai>
+Heian_Shodan_7_L2 Hidari <Zenkutsu Dachi>
+
+Heian_Shodan_8_L1 <Jodan Age Uke>
+Heian_Shodan_8_L2 Migi <Zenkutsu Dachi>
+
+Heian_Shodan_9_L1 <Jodan Age Uke>
+Heian_Shodan_9_L2 Hidari <Zenkutsu Dachi>
+
+Heian_Shodan_10_L1 <Jodan Age Uke>
+Heian_Shodan_10_L2 Migi <Zenkutsu Dachi>
+
+Heian_Shodan_11_L1 <Gedan Barai>
+Heian_Shodan_11_L2 Hidari <Zenkutsu Dachi>
+
+Heian_Shodan_12_L1 <<Oi Zuki>><Zuki>
+Heian_Shodan_12_L2 Migi <Zenkutsu Dachi>
+
+Heian_Shodan_13_L1 <Gedan Barai>
+Heian_Shodan_13_L2 Migi <Zenkutsu Dachi>
+
+Heian_Shodan_14_L1 <<Oi Zuki>><Zuki>
+Heian_Shodan_14_L2 Hidari <Zenkutsu Dachi>
+
+Heian_Shodan_15_L1 <Gedan Barai>
+Heian_Shodan_15_L2 Hidari <Zenkutsu Dachi>
+
+Heian_Shodan_16_L1 <<Oi Zuki>><Zuki>
+Heian_Shodan_16_L2 Migi <Zenkutsu Dachi>
+
+Heian_Shodan_17_L1 <<Oi Zuki>><Zuki>
+Heian_Shodan_17_L2 Hidari <Zenkutsu Dachi>
+
+Heian_Shodan_18_L1 <<Oi Zuki>><Zuki>
+Heian_Shodan_18_L2 Migi <Zenkutsu Dachi>
+
+Heian_Shodan_19_L1 <Shuto Uke>
+Heian_Shodan_19_L2 Hidari <Kokutsu Dachi>
+
+Heian_Shodan_20_L1 <Shuto Uke>
+Heian_Shodan_20_L2 Migi <Kokutsu Dachi>
+
+Heian_Shodan_21_L1 <Shuto Uke>
+Heian_Shodan_21_L2 Migi <Kokutsu Dachi>
+
+Heian_Shodan_22_L1 <Shuto Uke>
+Heian_Shodan_22_L2 Hidari <Kokutsu Dachi>
+
+Heian_Shodan_23_L1 <Yoi>
+Heian_Shodan_23_L2 <Hachiji Dachi>
+`;
