874154ca6a65fcf141d13a8a94b2860cf6501eb5
Kandaku <kandaku@gmail.com>
2025-12-13 21:09:23 +0100

    Heian Sandan

---
+++ techniques/Heian_Sandan/Heian_Sandan.js
@@ -0,0 +1,104 @@
+﻿// FICHIER DE CONFIGURATION DE LA TECHNIQUE Heian Sandan
+
+// ---------------------------------------------------------------
+// Instructions - Respectez la syntaxe window.VARIABLE = `valeur`;
+// avec les accents graves.
+// ---------------------------------------------------------------
+
+// 1. Extension des images (Optionnel)
+// Par défaut : .gif
+// Décommentez si vos images sont en .jpg ou .png
+// window.EXTENSION = `.jpg`;
+
+// 2. Nombre d'images (Obligatoire)
+window.COUNT = 26;
+
+// 3. Description (Optionnel)
+window.DESCRIPTION = `<b>Heian Sandan</b> est un kata du style <b>Shotokan</b>, servant de support à l'apprentissage progressif des déplacements, des positions et des techniques de base.<br><br>Son étude aide le pratiquant à structurer le travail du rythme, des changements de direction et de la coordination entre blocages et attaques, tout en développant stabilité et concentration.`;
+
+// 4. Arrière-plan (Optionnel)
+window.BACKGROUND = 1;
+
+// 5. Durée par défaut (Optionnel)
+window.SLIDEDURATION = 800;
+
+// 6. Données Techniques : Texte et Temps
+window.DONNEES = `
+Heian_Sandan_1_L1 <Yoi>
+Heian_Sandan_1_L2 <Hachiji Dachi>
+
+Heian_Sandan_2_L1 Chudan <Uchi Uke>
+Heian_Sandan_2_L2 Hidari <Kokutsu Dachi>
+
+Heian_Sandan_3_L1 Hidari <Gedan Barai> / Migi Chudan <Uchi Uke>
+Heian_Sandan_3_L2 <Heisoku Dachi>
+
+Heian_Sandan_4_L1 Migi <Gedan Barai> / Hidari Chudan <Uchi Uke>
+Heian_Sandan_4_L2 <Heisoku Dachi>
+
+Heian_Sandan_5_L1 Chudan <Uchi Uke>
+Heian_Sandan_5_L2 Migi <Kokutsu Dachi>
+
+Heian_Sandan_6_L1 Migi <Gedan Barai> / Hidari Chudan <Uchi Uke>
+Heian_Sandan_6_L2 <Heisoku Dachi>
+
+Heian_Sandan_7_L1 Hidari <Gedan Barai> / Migi Chudan <Uchi Uke>
+Heian_Sandan_7_L2 <Heisoku Dachi>
+
+Heian_Sandan_8_L1 Chudan <Morote Uke>
+Heian_Sandan_8_L2 Hidari <Kokutsu Dachi>
+
+Heian_Sandan_9_L1 Chudan Tate <Nukite> / Gyaku <Te Osae Uke>
+Heian_Sandan_9_L2 Migi <Zenkutsu Dachi>
+
+Heian_Sandan_10_L1 Hidari Chudan Yoko <Tetsui> Uchi
+Heian_Sandan_10_L2 <Kiba Dachi>
+
+Heian_Sandan_11_L1 Chudan <<Oi Zuki>><Zuki>
+Heian_Sandan_11_L2 Migi <Zenkutsu Dachi>
+
+Heian_Sandan_12_L1 <Ryoken Koshi Gamae>
+Heian_Sandan_12_L2 <Heisoku Dachi>
+
+Heian_Sandan_13_L1 Migi Yoko <Fumikomi>
+Heian_Sandan_13_L2 
+
+Heian_Sandan_14_L1 Migi Yoko <Empi Uke>
+Heian_Sandan_14_L2 <Kiba Dachi>
+
+Heian_Sandan_15_L1 Migi Jodan Tate <Uraken> Uchi
+Heian_Sandan_15_L2 <Kiba Dachi>
+
+Heian_Sandan_16_L1 Hidari Yoko <Fumikomi>
+Heian_Sandan_16_L2
+
+Heian_Sandan_17_L1 Hidari Yoko <Empi Uke>
+Heian_Sandan_17_L2 <Kiba Dachi>
+
+Heian_Sandan_18_L1 Hidari Jodan Tate <Uraken> Uchi
+Heian_Sandan_18_L2 <Kiba Dachi>
+
+Heian_Sandan_19_L1 Migi Yoko <Fumikomi>
+Heian_Sandan_19_L2
+
+Heian_Sandan_20_L1 Migi Yoko <Empi Uke>
+Heian_Sandan_20_L2 <Kiba Dachi>
+
+Heian_Sandan_21_L1 Migi Jodan Tate <Uraken> Uchi
+Heian_Sandan_21_L2 <Kiba Dachi>
+
+Heian_Sandan_22_L1 Migi Chudan <Tate Shuto Uke>
+Heian_Sandan_22_L2 <Kiba Dachi>
+
+Heian_Sandan_23_L1 Chudan <<Oi Zuki>><Zuki>
+Heian_Sandan_23_L2 Hidari <Zenkutsu Dachi>
+
+Heian_Sandan_24_L1 Migi Jodan Mawashi <Zuki>
+Heian_Sandan_24_L2 <Kiba Dachi>
+
+Heian_Sandan_25_L1 Hidari Jodan Mawashi <Zuki>
+Heian_Sandan_25_L2 <Kiba Dachi>
+
+Heian_Sandan_26_L1 <Yoi>
+Heian_Sandan_26_L2 <Hachiji Dachi>
+`;
