bf13cd987d23352728c9ca776c6aef5e329179cb
Kandaku <kandaku@gmail.com>
2025-12-13 21:11:23 +0100

    Heian Yondan

---
+++ techniques/Heian_Yondan/Heian_Yondan.js
@@ -0,0 +1,138 @@
+﻿// FICHIER DE CONFIGURATION DE LA TECHNIQUE Heian Yodan
+
+// ---------------------------------------------------------------
+// Instructions - Respectez la syntaxe window.VARIABLE = `valeur`;
+// avec les accents graves.
+// ---------------------------------------------------------------
+
+// 1. Extension des images (Optionnel)
+// Par défaut : .gif
+// Décommentez si vos images sont en .jpg ou .png
+// window.EXTENSION = `.jpg`;
+
+// 2. Nombre d'images (Obligatoire)
+window.COUNT = 37;
+
+// 3. Description (Optionnel)
+window.DESCRIPTION = `<b>Heian Yodan</b> est un kata du style <b>Shotokan</b>, servant de support à l'apprentissage progressif des déplacements, des positions et des techniques de base.<br><br>Son étude aide le pratiquant à structurer le travail du rythme, des changements de direction et de la coordination entre blocages et attaques, tout en développant stabilité et concentration.`;
+
+// 4. Arrière-plan (Optionnel)
+window.BACKGROUND = 1;
+
+// 5. Durée par défaut (Optionnel)
+window.SLIDEDURATION = 800;
+
+// 6. Données Techniques : Texte et Temps
+window.DONNEES = `
+Heian_Yondan_1_L1 <Yoi>
+Heian_Yondan_1_L2 <Hachiji Dachi>
+
+Heian_Yondan_2_L1 <Haishu> <Haiwan Sokumen Uke>
+Heian_Yondan_2_L2 Hidari <Kokutsu Dachi>
+
+Heian_Yondan_3_L1 <Haishu> <Haiwan Sokumen Uke>
+Heian_Yondan_3_L2 Migi <Kokutsu Dachi>
+
+Heian_Yondan_4_L1 Gedan <Juji Uke>
+Heian_Yondan_4_L2 Hidari <Zenzutsu Dachi>
+
+Heian_Yondan_5_L1 Chudan <Morote Uke>
+Heian_Yondan_5_L2 Migi <Kokutsu Dachi>
+
+Heian_Yondan_6_L1 <Koshi Gamae>
+Heian_Yondan_6_L2
+
+Heian_Yondan_7_L1 Hidari Jodan Yoko Mawashi <Uraken Uchi>
+Heian_Yondan_7_L2
+
+Heian_Yondan_8_L1 Gyaku <Mae Empi Uchi> Chudan
+Heian_Yondan_8_L2 Hidari <Zenkutsu Dachi>
+
+Heian_Yondan_9_L1 <Koshi Gamae>
+Heian_Yondan_9_L2 Migi <Tsuru Ashi Dachi>
+
+Heian_Yondan_10_L1 Hidari Jodan Yoko Mawashi <Uraken Uchi>
+Heian_Yondan_10_L2 
+                   
+Heian_Yondan_11_L1 Hidari Jodan Yoko Mawashi <Uraken Uchi>
+Heian_Yondan_11_L2 
+                   
+Heian_Yondan_12_L1 Hidari Jodan Yoko Mawashi <Uraken Uchi>
+Heian_Yondan_12_L2 
+                   
+Heian_Yondan_13_L1 Gyaku <Mae Empi Uchi> Chudan
+Heian_Yondan_13_L2 Hidari <Zenkutsu Dachi>
+                   
+Heian_Yondan_14_L1 Gyaku <Mae Empi Uchi> Chudan
+Heian_Yondan_14_L2 Hidari <Zenkutsu Dachi>
+                   
+Heian_Yondan_15_L1 <Koshi Gamae>
+Heian_Yondan_15_L2 
+                   
+Heian_Yondan_16_L1 Hidari Jodan Yoko Mawashi <Uraken> Uchi // Hidari <Yoko Geri> Keage
+Heian_Yondan_16_L2 
+                   
+Heian_Yondan_17_L1 Gyaku <Mae Empi Uchi> Chudan
+Heian_Yondan_17_L2 Migi <Zenkutsu Dachi>
+                   
+Heian_Yondan_18_L1 Hikite "à la tempe" // Gyaku Gedan <Shuto> Barai
+Heian_Yondan_18_L2 Migi <Zenkutsu Dachi>
+                   
+Heian_Yondan_19_L1 Jodan <Shuto Uke> // Gyaku Jodan <Shuto> Uchi
+Heian_Yondan_19_L2 Hidari <Zenkutsu Dachi>
+                   
+Heian_Yondan_20_L1 Migi <Mae Geri>
+Heian_Yondan_20_L2 
+                   
+Heian_Yondan_21_L1 Chudan Tate <Uraken> Uchi
+Heian_Yondan_21_L2 Migi <Kake Dachi>
+                   
+Heian_Yondan_22_L1 Chudan <Kakiwake Uke>
+Heian_Yondan_22_L2 Hidari <Kokutsu Dachi>
+                   
+Heian_Yondan_23_L1 Migi Chudan <Mae Geri>
+Heian_Yondan_23_L2 
+                   
+Heian_Yondan_24_L1 Chudan <<Oi Zuki>><Zuki>
+Heian_Yondan_24_L2 Migi <Zenkutsu Dachi>
+                   
+Heian_Yondan_25_L1 
+Heian_Yondan_25_L2 Gyaku <Zuki>
+                   Migi <Zenkutsu Dachi>
+                   
+Heian_Yondan_26_L1 Chudan <Kakiwake Uke>
+Heian_Yondan_26_L2 Migi <Kokutsu Dachi>
+                   
+Heian_Yondan_27_L1 Hidari Chudan <Mae Geri>
+Heian_Yondan_27_L2 
+                   
+Heian_Yondan_28_L1 Chudan <<Oi Zuki>><Zuki>
+Heian_Yondan_28_L2 Hidari <Zenkutsu Dachi>
+                   
+Heian_Yondan_29_L1 Gyaku <Zuki>
+Heian_Yondan_29_L2 Hidari <Zenkutsu Dachi>
+                   
+Heian_Yondan_30_L1 Chudan <Morote Uke>
+Heian_Yondan_30_L2 Hidari <Kokutsu Dachi>
+                   
+Heian_Yondan_31_L1 Chudan <Morote Uke>
+Heian_Yondan_31_L2 Migi <Kokutsu Dachi>
+                   
+Heian_Yondan_32_L1 Chudan <Morote Uke>
+Heian_Yondan_32_L2 Hidari <Kokutsu Dachi>
+                   
+Heian_Yondan_33_L1 "Saisie de la tête de l'adversaire"
+Heian_Yondan_33_L2 Hidari <Zenkutsu Dachi>
+                   
+Heian_Yondan_34_L1 Migi <Hiza Geri>
+Heian_Yondan_34_L2 
+                   
+Heian_Yondan_35_L1 Chudan <Shuto Uke>
+Heian_Yondan_35_L2 Hidari <Kokutsu Dachi>
+                   
+Heian_Yondan_36_L1 Chudan <Shuto Uke>
+Heian_Yondan_36_L2 Migi <Kokutsu Dachi>
+                   
+Heian_Yondan_37_L1 <Yoi>
+Heian_Yondan_37_L2 <Hachiji Dachi>
+`;
