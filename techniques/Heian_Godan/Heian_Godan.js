18bb460a1185f83dcaf284892930d22ff6816d5d
Kandaku <kandaku@gmail.com>
2025-12-13 21:07:16 +0100

    Add files via upload

---
+++ techniques/Heian_Godan/Heian_Godan.js
@@ -0,0 +1,128 @@
+﻿// FICHIER DE CONFIGURATION DE LA TECHNIQUE Heian Godan
+
+// ---------------------------------------------------------------
+// Instructions - Respectez la syntaxe window.VARIABLE = `valeur`;
+// avec les accents graves.
+// ---------------------------------------------------------------
+
+// 1. Extension des images (Optionnel)
+// Par défaut : .gif
+// Décommentez si vos images sont en .jpg ou .png
+window.EXTENSION = `.png`;
+
+// 2. Nombre d'images (Obligatoire)
+window.COUNT = 34;
+
+// 3. Description (Optionnel)
+window.DESCRIPTION = `<b>Heian Godan</b> est un kata du style <b>Shotokan</b>, servant de support à l'apprentissage progressif des déplacements, des positions et des techniques de base.<br><br>Son étude aide le pratiquant à structurer le travail du rythme, des changements de direction et de la coordination entre blocages et attaques, tout en développant stabilité et concentration.`;
+
+// 4. Arrière-plan (Optionnel)
+window.BACKGROUND = 1;
+
+// 5. Durée par défaut (Optionnel)
+window.SLIDEDURATION = 800;
+
+// 6. Données Techniques : Texte et Temps
+window.DONNEES = `
+Heian_Godan_1_L1 <Yoi>
+Heian_Godan_1_L2 <Hachiji Dachi>
+
+Heian_Godan_2_L1 Chudan <Uchi Uke>
+Heian_Godan_2_L2 Hidari <Kokutsu Dachi>
+
+Heian_Godan_3_L1 Chudan Gyaku <Zuki>
+Heian_Godan_3_L2 Hidari <Kokutsu Dachi>
+
+Heian_Godan_4_L1 Hidari <Kagi Zuki>
+Heian_Godan_4_L2 <Heisoku Dachi>
+
+Heian_Godan_5_L1 Chudan <Uchi Uke>
+Heian_Godan_5_L2 Hidari <Kokutsu Dachi>
+
+Heian_Godan_6_L1 Chudan Gyaku <Zuki>
+Heian_Godan_6_L2 Hidari <Kokutsu Dachi>
+
+Heian_Godan_7_L1 Hidari <Kagi Zuki>
+Heian_Godan_7_L2 <Heisoku Dachi>
+
+Heian_Godan_8_L1 Chudan <Morote Uke>
+Heian_Godan_8_L2 Migi <Kokutsu Dachi>
+
+Heian_Godan_9_L1 Gedan <Juji Uke>
+Heian_Godan_9_L2 Hidari <Zenkutsu Dachi>
+
+Heian_Godan_10_L1 Jodan <Juji Uke>
+Heian_Godan_10_L2 Hidari <Zenkutsu Dachi>
+
+Heian_Godan_11_L1 Chudan <<Te Osae Uke>><Osae Uke>
+Heian_Godan_11_L2 Hidari <Zenkutsu Dachi>
+
+Heian_Godan_12_L1 Chudan <Zuki>
+Heian_Godan_12_L2 Hidari <Zenkutsu Dachi>
+
+Heian_Godan_13_L1 Chudan <<Oi Zuki>><Zuki>
+Heian_Godan_13_L2 Migi <Zenkutsu Dachi>
+
+Heian_Godan_14_L1 Migi Yoko <Gedan Barai>
+Heian_Godan_14_L2 <Kiba Dachi>
+
+Heian_Godan_15_L1 Hidari Chudan <Haishu> Uke
+Heian_Godan_15_L2 <Kiba Dachi>
+
+Heian_Godan_16_L1 Migi Chudan <Mikazuki Geri>
+Heian_Godan_16_L2
+
+Heian_Godan_17_L1 Migi Chudan <Mae Empi Uchi>
+Heian_Godan_17_L2 <Kiba Dachi>
+
+Heian_Godan_18_L1 Chudan <Morote Uke>
+Heian_Godan_18_L2 Migi <Kake Dachi>
+
+Heian_Godan_19_L1 Gyaku Ushiro Jodan Ura <Zuki>
+Heian_Godan_19_L2 Hidari <Renoji Dachi>
+
+Heian_Godan_20_L1 Gedan <Juji Uke>
+Heian_Godan_20_L2 Migi <Kake Dachi>
+
+Heian_Godan_21_L1 Gyaku Ushiro Jodan Ura <Zuki>
+Heian_Godan_21_L2 Hidari <Renoji Dachi>
+
+Heian_Godan_22_L1 
+Heian_Godan_22_L2
+
+Heian_Godan_23_L1 
+Heian_Godan_23_L2 
+
+Heian_Godan_24_L1 
+Heian_Godan_24_L2 
+
+Heian_Godan_25_L1 
+Heian_Godan_25_L2 
+
+Heian_Godan_26_L1 
+Heian_Godan_26_L2
+
+Heian_Godan_27_L1 Chudan <Morote Uke>
+Heian_Godan_27_L2 Migi <Zenkutsu Dachi>
+
+Heian_Godan_28_L1 Nagashi Jodan <Teisho Uke> -  Gyaku Gedan Ura <Nukite>
+Heian_Godan_28_L2 Hidari <Zenkutsu Dachi>
+
+Heian_Godan_29_L1 <Manji Uke>
+Heian_Godan_29_L2 Hidari <Kokutsu Dachi>
+
+Heian_Godan_30_L1 Hidari <<Manji Kamae>><Manji Uke>
+Heian_Godan_30_L2 <Heisoku Dachi>
+
+Heian_Godan_31_L1 Migi <<Manji Kamae>><Manji Uke>
+Heian_Godan_31_L2 <Heisoku Dachi>
+
+Heian_Godan_32_L1 Nagashi Jodan <Teisho Uke> -  Gyaku Gedan Ura <Nukite>
+Heian_Godan_32_L2 Migi <Zenkutsu Dachi>
+
+Heian_Godan_33_L1 <Manji Uke>
+Heian_Godan_33_L2 Migi <Kokutsu Dachi>
+
+Heian_Godan_34_L1 <Yoi>
+Heian_Godan_34_L2 <Hachiji Dachi>                  
+`;
