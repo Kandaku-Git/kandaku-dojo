143dc07126f56d85aeceb0ef9e1980a290b8c6e8
Kandaku <kandaku@gmail.com>
2025-12-13 01:46:30 +0100

    Yoko Geri

---
+++ techniques/Yoko_Geri/Yoko_Geri.js
@@ -0,0 +1,43 @@
+﻿/**
+
+* FICHIER DE CONFIGURATION DE LA TECHNIQUE : Yoko Geri
+
+*/
+
+// --- 1. Extension des images ---
+// Par défaut `.gif`. Décommentez si différent.
+// window.EXTENSION = `.gif`;
+
+// --- 2. Nombre d'images ---
+window.COUNT = `3`;
+
+// --- 3. Description ---
+window.DESCRIPTION = `
+<center><b>Coup de pied latéral</b></center>
+<br>
+<br>
+
+<b> Armer :</b>
+<br>
+. Le pied qui va frapper se trouve au-dessus de l'autre genou
+<br>
+<br>
+<b> Développer : </b>
+<br>
+. La trajectoire est rectiligne, pour une frappe pénétrante (Yoko Geri Kekomi)
+<br>
+. La trajectoire est circulaire, sur un plan verical, pour une frappe fouéttée (Yoko Geri Keage)
+<br>
+<br>
+<b> Résultat : </b>
+<br>
+. Un coup de pied (Geri) a été porté de façon latérale (Yoko)
+<br>
+. Afin de le reposer, suivre la trajectoire inverse de précédemment
+`;
+
+// --- 4. Données Techniques ---
+window.DONNEES = `
+FIXE_L1 Geri Waza
+FIXE_L2 Technique de jambe
+`;
