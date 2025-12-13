6fff1c149777dd335578d694c3011e2783f9c1ad
Kandaku <kandaku@gmail.com>
2025-12-13 21:06:10 +0100

    Add files via upload

---
+++ techniques/Kakiwake_Uke/Kakiwake_Uke.js
@@ -0,0 +1,46 @@
+﻿/**
+
+* FICHIER DE CONFIGURATION DE LA TECHNIQUE : Kakiwake Uke
+
+*/
+
+// --- 1. Extension des images ---
+// Par défaut `.gif`. Décommentez si différent.
+// window.EXTENSION = `.gif`;
+
+// --- 2. Nombre d'images ---
+window.COUNT = `2`;
+
+// --- 3. Description ---
+window.DESCRIPTION = `
+<center><b>Blocage double en écartant</b></center>
+<br>
+<br>
+<b> Armer :</b>
+<br>
+. Les deux poings sont en hikite
+<br>
+<br>
+<b> Développer : </b>
+<br>
+1. Les deux poings remontent à la verticale :
+<br>
+-- à la fin de la montée, les poings se croisent
+<br>
+-- le dos des mains est tourné vers l'adversaire
+<br>
+2. Les poings s'écartent en descendant légèrement:
+<br>
+-- le dos des mains est tourné vers soi
+<br>
+<br>
+<b> Résultat : </b>
+<br>
+. Un double blocage a été effectué 
+`;
+
+// --- 4. Données Techniques ---
+window.DONNEES = `
+FIXE_L1 Uke Waza
+FIXE_L2 Technique de défense
+`;
---
+++ techniques/Manji_Uke/Manji_Uke.js
@@ -0,0 +1,37 @@
+﻿/**
+
+* FICHIER DE CONFIGURATION DE LA TECHNIQUE : Manji Uke
+
+*/
+
+// --- 1. Extension des images ---
+// Par défaut `.gif`. Décommentez si différent.
+// window.EXTENSION = `.gif`;
+
+// --- 2. Nombre d'images ---
+window.COUNT = `1`;
+
+// --- 3. Description ---
+window.DESCRIPTION = `
+<center><b>Défense haute (sur l'arrière) et basse (sur l'avant)</b></center>
+<br>
+<br>
+
+C'est un double blocage :
+<br>
+. Jodan <b><a class=kata_technique href="d_uchiuke.html" onClick="newWindow(410,770)" target="coverwindow"> Uchi Uke </a></b> sur l'arrière
+<br>
+. <b><a class=kata_technique href="d_gedanbarai.html" onClick="newWindow(310,740)" target="coverwindow"> Gedan Barai </a></b> sur l'avant
+		<span class="technique_corps">
+<br>
+<br>
+<b> Remarque : </b>
+<br>
+<span class="technique_variante">Manji Kamae</b> : position finale de la technique <span class="technique_variante">Manji Uke</b>
+`;
+
+// --- 4. Données Techniques ---
+window.DONNEES = `
+FIXE_L1 Uke Waza
+FIXE_L2 Technique de défense
+`;
