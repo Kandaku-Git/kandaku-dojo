9653b40429524f032548ac7c65600d0857c859f4
Kandaku <kandaku@gmail.com>
2025-12-13 01:13:54 +0100

    Heian Nidan

---
+++ techniques/Heian_Nidan/Heian_Nidan.js
@@ -0,0 +1,91 @@
+﻿// FICHIER DE CONFIGURATION DE LA TECHNIQUE Heian Nidan
+
+// ---------------------------------------------------------------
+// Instructions - Respectez la syntaxe window.VARIABLE = `valeur`;
+// avec les accents graves.
+// ---------------------------------------------------------------
+
+// 1. Extension des images (Optionnel)
+// Par défaut : .gif
+// Décommentez si vos images sont en .jpg ou .png
+// window.EXTENSION = `.jpg`;
+
+// 2. Nombre d'images (Obligatoire)
+window.COUNT = 32;
+
+// 3. Description (Optionnel)
+window.DESCRIPTION = `<b>Heian Nidan</b> est un kata du style <b>Shotokan</b>, servant de support à l'apprentissage progressif des déplacements, des positions et des techniques de base.<br><br>Son étude aide le pratiquant à structurer le travail du rythme, des changements de direction et de la coordination entre blocages et attaques, tout en développant stabilité et concentration.`;
+
+// 4. Arrière-plan (Optionnel)
+window.BACKGROUND = 1;
+
+// 5. Durée par défaut (Optionnel)
+window.SLIDEDURATION = 800;
+
+// 6. Données Techniques : Texte et Temps
+window.DONNEES = `
+Heian_Nidan_1_L1 <Yoi>
+Heian_Nidan_1_L2 <Hachiji Dachi>
+Heian_Nidan_2_L1 <Haiwan Sokumen Uke>
+Heian_Nidan_2_L2 Hidari <Kokutsu Dachi>
+Heian_Nidan_3_L1 Gyaku <<Ura Zuki>><Zuki> / <<Uchi Komi>><Soto Uke>
+Heian_Nidan_3_L2 Hidari <Kokutsu Dachi>
+Heian_Nidan_4_L1 Chudan Yoko <Zuki>
+Heian_Nidan_4_L2 Hidari <Kokutsu Dachi>
+Heian_Nidan_5_L1 
+Heian_Nidan_5_L2 
+Heian_Nidan_6_L1 <Haiwan Sokumen Uke>
+Heian_Nidan_6_L2 Migi <Kokutsu Dachi>
+Heian_Nidan_7_L1 Gyaku <Ura Zuki> / <<Uchi Komi>><Soto Uke>
+Heian_Nidan_7_L2 Migi <Kokutsu Dachi>
+Heian_Nidan_8_L1 Chudan Yoko <Zuki>
+Heian_Nidan_8_L2 Migi <Kokutsu Dachi>
+Heian_Nidan_9_L1 
+Heian_Nidan_9_L2 
+Heian_Nidan_10_L1 Migi Jodan <<Uraken> Uchi>><Uraken> / Migi <Yoko Geri> Keage
+Heian_Nidan_10_L2 
+Heian_Nidan_11_L1 <Shuto Uke>
+Heian_Nidan_11_L2 Hidari <Kokutsu Dachi>
+Heian_Nidan_12_L1 <Shuto Uke>
+Heian_Nidan_12_L2 Migi <Kokutsu Dachi>
+Heian_Nidan_13_L1 <Shuto Uke>
+Heian_Nidan_13_L2 Hidari <Kokutsu Dachi>
+Heian_Nidan_14_L1 Gyaku <Te Osae Uke> / <<Tate Nukite>><Nukite>
+Heian_Nidan_14_L2 Migi <Zenkutsu Dachi>
+Heian_Nidan_15_L1 <Shuto Uke>
+Heian_Nidan_15_L2 Hidari <Kokutsu Dachi>
+Heian_Nidan_16_L1 <Shuto Uke>
+Heian_Nidan_16_L2 Migi <Kokutsu Dachi>
+Heian_Nidan_17_L1 <Shuto Uke>
+Heian_Nidan_17_L2 Migi <Kokutsu Dachi>
+Heian_Nidan_18_L1 <Shuto Uke>
+Heian_Nidan_18_L2 Hidari <Kokutsu Dachi>
+Heian_Nidan_19_L1 Gyaku <Uchi Uke>
+Heian_Nidan_19_L2 Hidari <Zenkutsu Dachi>
+Heian_Nidan_20_L1 Migi <Mae Geri> Keage
+Heian_Nidan_20_L2 
+Heian_Nidan_21_L1 Chudan Gyaku <Zuki>
+Heian_Nidan_21_L2 Migi <Zenkutsu Dachi>
+Heian_Nidan_22_L1 Gyaku <Uchi Uke>
+Heian_Nidan_22_L2 Migi <Zenkutsu Dachi>
+Heian_Nidan_23_L1 Hidari <Mae Geri> Keage
+Heian_Nidan_23_L2 
+Heian_Nidan_24_L1 Gyaku <Zuki>
+Heian_Nidan_24_L2 Hidari <Zenkutsu Dachi>
+Heian_Nidan_25_L1 Chudan <Morote Uke>
+Heian_Nidan_25_L2 Migi <Zenkutsu Dachi>
+Heian_Nidan_26_L1 <Gedan Barai>
+Heian_Nidan_26_L2 Hidari <Zenkutsu Dachi>
+Heian_Nidan_27_L1 
+Heian_Nidan_27_L2 
+Heian_Nidan_28_L1 <Jodan Age Uke>
+Heian_Nidan_28_L2 Migi <Zenkutsu Dachi>
+Heian_Nidan_29_L1 <Gedan Barai>
+Heian_Nidan_29_L2 Migi <Zenkutsu Dachi>
+Heian_Nidan_30_L1 
+Heian_Nidan_30_L2 
+Heian_Nidan_31_L1 <Jodan Age Uke>
+Heian_Nidan_31_L2 Hidari <Zenkutsu Dachi>
+Heian_Nidan_32_L1 Yoi
+Heian_Nidan_32_L2 <Hachiji Dachi>
+`;
