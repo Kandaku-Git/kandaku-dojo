fa479498466dd52c7b076030a9c507c4132207ad
Kandaku <kandaku@gmail.com>
2026-01-03 02:10:18 +0100

    Add files via upload

---
+++ techniques/Kakete_Uke/Kakete_Uke.js
@@ -0,0 +1,42 @@
+﻿/**
+
+* FICHIER DE CONFIGURATION DE LA TECHNIQUE : Kakete Uke
+
+*/
+
+// --- 1. Extension des images ---
+// Par défaut `.gif`. Décommentez si différent.
+// window.EXTENSION = `.gif`;
+
+// --- 2. Nombre d'images ---
+window.COUNT = `1`;
+
+// --- 3. Description ---
+window.DESCRIPTION = `
+<center><b>Main crochetant</b></center>
+<br>
+<br>
+<b> Armer :</b>
+<br>
+. La paume de la main crochetant est dirigée vers le bas
+<br>
+<br>
+<b> Développer : </b>
+<br>
+. Le bras va chercher le contact loin devant pour intercepter l'attaque
+<br>
+. La main reste dans le prolongement de l'avant-bras
+<br>
+<br>
+<b> Résultat : </b>
+<br>
+. Une fois le contact établi, le pouce se verrouille pour crocheter
+<br>
+. Une traction est exercée vers soi pour contrôler ou déséquilibrer l'adversaire
+`;
+
+// --- 4. Données Techniques ---
+window.DONNEES = `
+FIXE_L1 Uke Waza
+FIXE_L2 Technique de défense
+`;
